11d04dae8672f96a17ca0f7cac5258b1a5440fa1
belinskyc <belinsky@msu.edu>
2026-06-24 09:12:50 -0400

    error 2 try 2

--- scripts/script3.R
+++ scripts/script3.R
@@ -1,6 +1,7 @@
 rm(list=ls());                         # clear Environment tab
 library(package=ggplot2);              # get the GGPlot package
 
+### Change for error 2
 #### Use application from boxplots2 #####
 
 # read in CSV file and save the content to weatherData
